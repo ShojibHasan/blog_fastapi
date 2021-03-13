2ecf3a34fb728b512230f995cdaae9c1c5a28883
shojibhasan <shojibhasan15@gmail.com>
2021-03-13 13:37:40 +0600

    user account creat api

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI,Depends,status,Response,HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm.session import Session
@@ -27,9 +28,13 @@ def create(request: schemas.Blog, db: Session= Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def distroy(id, db: Session=Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} not found')
+
+    blog.delete(synchronize_session=False)
     db.commit()
-    return {'detail': {'deleted'}}
+    return 'done'
 
 @app.put('/blog/{id}')
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
@@ -41,7 +46,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'updated'
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session= Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -54,3 +59,12 @@ def show(id, response: Response, db: Session=Depends(get_db)):
         # response.status_code=status.HTTP_404_NOT_FOUND
         # return{'detail': f'Blog with the id {id} is not available' }
     return blog
+
+
+@app.post('/user',status_code=status.HTTP_201_CREATED)
+def create_user(request: schemas.User,db: Session=Depends(get_db)):
+    new_user = models.User(name=request.name,email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
